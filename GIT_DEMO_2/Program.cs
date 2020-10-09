65507eddc0500bbbd4d8f9d143a2ad7675b1d6cf
zak <zak@mac.fr>
2020-10-09 09:10:34 +0200

    Add project files.

---
+++ GIT_DEMO_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GIT_DEMO_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
